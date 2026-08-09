992823cd0319dcf73e7cfa05c01bb993b726c38f
OpuTheCoder <artistop26@gmail.com>
2026-08-09 18:41:22 +0600

    feat: add next-auth API route handler for authentication endpoints

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,6 @@
-import { authOptions } from "@/lib/authOption"
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/authOption";
 
 const handler = NextAuth(authOptions);
-
 
 export { handler as GET, handler as POST };
